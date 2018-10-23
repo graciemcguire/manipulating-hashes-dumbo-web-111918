64041bc0112539feb474a66c300c76d9c34aea68
Gracie McGuire <m.graciemcguire@gmail.com>
2018-10-23 21:16:24 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,7 @@ def first_challenge
     data.each do |attribute, value|
       if attribute == :favorite_icecream_flavors
         value.each do |flavor|
-          flavor.delete_if  == "strawberry"
+          flavor.delete  == "strawberry"
           
           end
         end
